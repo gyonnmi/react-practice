b343b7a0efaf028a954f4312d862575c7347b1e3
gyonnmi <107240999+gyonnmi@users.noreply.github.com>
2022-10-28 00:38:11 +0900

    refactor: useRef

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,24 +1,46 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
 
+function Backdrop(props) {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+}
+
+function ModalOverlay(props) {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>확인</Button>
+      </footer>
+    </Card>
+  );
+}
+
 function ErrorModal(props) {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>확인</Button>
-        </footer>
-      </Card>
-    </div>
+    <React.Fragment>
+      {/* createPortal(렌더링되어야 하는 리액트 노드, 포인터(화살표)) */}
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.querySelector("#backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.querySelector("#overlay-root")
+      )}
+    </React.Fragment>
   );
 }
 
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,21 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useRef } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 import classes from "./AddUser.module.css";
 
 function AddUser(props) {
-  const [inputUserName, setInputUserName] = useState("");
-  const [inputUserAge, setInputUserAge] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
 
   const addUserHandler = (e) => {
     e.preventDefault();
-    if (inputUserName.trim().length === 0 || inputUserAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: "Invalid input",
         message: "Empty values. 공백은 입력할 수 없습니다.",
@@ -20,23 +23,16 @@ function AddUser(props) {
       return; //함수 실행 종료
     }
     // +단항연산자(문자를 숫자로), input으로 들어온 값은 문자열로 반환됨
-    if (+inputUserAge < 1) {
+    if (+enteredAge < 1) {
       setError({
         title: "Invalid age",
         message: "유효하지 않은 값 입니다. (0이상의 정수만 입력 가능)",
       });
       return;
     }
-    props.onAddUser(inputUserName, inputUserAge);
-    setInputUserName("");
-    setInputUserAge("");
-  };
-
-  const userNameChangeHandler = (e) => {
-    setInputUserName(e.target.value);
-  };
-  const userAgeChangeHandler = (e) => {
-    setInputUserAge(e.target.value);
+    props.onAddUser(enteredName, enteredAge);
+    nameInputRef.current.value(""); //초기화
+    ageInputRef.current.value("");
   };
 
   const errorHandler = () => {
@@ -44,7 +40,7 @@ function AddUser(props) {
   };
 
   return (
-    <div>
+    <Wrapper>
       {/* 조건부 렌더링 */}
       {error && (
         <ErrorModal
@@ -57,23 +53,13 @@ function AddUser(props) {
         <form onSubmit={addUserHandler}>
           {/* <label for="결합될 요소 id"></label> */}
           <label htmlFor="userName">이름</label>
-          <input
-            type="text"
-            id="userName"
-            value={inputUserName}
-            onChange={userNameChangeHandler}
-          />
+          <input type="text" id="userName" ref={nameInputRef} />
           <label htmlFor="userAge">나이</label>
-          <input
-            type="number"
-            id="userAge"
-            value={inputUserAge}
-            onChange={userAgeChangeHandler}
-          />
+          <input type="number" id="userAge" ref={ageInputRef} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 }
 
